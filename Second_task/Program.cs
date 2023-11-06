2cbebdb737310e4d02667858226478bdcd0306b4
Богдан Безуглый <winzar06031977@gmail.com>
2023-11-06 13:54:52 +0200

    Add project files.

---
+++ Second_task/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+
+namespace Second_task
+{
+    interface IParent
+    {
+        string Info();
+        int Method(int x);
+    }
+    class Child1 : IParent
+    {
+        int b,c;
+        public Child1(int b ,int c)
+        {
+            this.c = c;
+            this.b = b;
+        }
+        public string Info()
+        {
+            return $"Лінійна функція: y = {b} * x + {c}";
+        }
+
+        public int Method(int x)
+        {
+            return b * x + c ;
+        }
+    }
+    class Child2 : IParent
+    {
+        int a, b, c;
+        public Child2(int a,int b,int c)
+        {
+            this.c = c;
+            this.b = b;
+            this.a = a;
+        }
+        public string Info()
+        {
+            return $"Квадратична функція: y = {a} * x * x + {b} * x + {c}";
+        }
+
+        public int Method(int x)
+        {
+            return a * x * x + b * x + c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int ChoiseChild = rand.Next(2);
+                if (ChoiseChild == 1)
+                {
+                    int x = rand.Next(300);
+                    int b = rand.Next(300);
+                    int c = rand.Next(300);
+                    Child1 child1 = new Child1(b,c);
+                    Console.WriteLine(child1.Info());
+                    Console.WriteLine($"При x = {x} y = " + child1.Method(x));
+                }
+                else
+                {
+                    int x = rand.Next(300);
+                    int a = rand.Next(300);
+                    int b = rand.Next(300);
+                    int c = rand.Next(300);
+                    Child2 child2 = new Child2(a, b, c);
+                    Console.WriteLine(child2.Info());
+                    Console.WriteLine($"При x = {x} y = " + child2.Method(x));
+                }
+            }
+        }
+    }
+}
---
+++ laboratory_work_5_OOP/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace laboratory_work_5_OOP
+{
+    abstract class Parent
+    {
+        protected double pole1;//Швидкість
+        protected double pole2;//Собівартість
+        protected double pole3;//Вартість
+        public Parent(double pole1,double pole2)
+        {
+            this.pole1 = pole1;
+            this.pole2 = pole2;
+        }
+        public abstract string Info();
+        public abstract void Method();
+    }
+    class Child1 : Parent
+    {
+        public Child1(double pole1, double pole2) : base(pole1, pole2)
+        {
+
+        }
+        public override string Info()
+        {
+            return $"Морський транспорт:\n Швидкість: {pole1:F2};\n Собівартість: {pole2:F2};\n Вартість: {pole3:F2}\n";
+        }
+
+        public override void Method()
+        {
+            pole3 = pole2 / pole1;
+        }
+    }
+    class Child2 : Parent
+    {
+        double pole4;//Дорожній збір
+        public Child2(double pole1, double pole2, double pole4) : base(pole1, pole2)
+        {
+            this.pole4 = pole4;
+        }
+        public override string Info()
+        {
+            return $"Наземний транспорт:\n Швидкість: {pole1:F2};\n Собівартість: {pole2:F2};\n Вартість: {pole3:F2};\n Дорожній збір: {pole4:F2}\n";
+        }
+
+        public override void Method()
+        {
+            pole3 = (pole2 / pole1) + pole4;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int ChoiseChild = rand.Next(2);
+                if(ChoiseChild == 1)
+                {
+                    double pole1 = rand.Next(3000) + rand.NextDouble();
+                    double pole2 = rand.Next(3000) + rand.NextDouble();
+                    Child1 child1 = new Child1(pole1, pole2);
+                    child1.Method();
+                    Console.WriteLine(child1.Info()); 
+                }
+                else
+                {
+                    double pole1 = rand.Next(3000) + rand.NextDouble();
+                    double pole2 = rand.Next(3000) + rand.NextDouble();
+                    double pole4 = rand.Next(3000) + rand.NextDouble();
+                    Child2 child2 = new Child2(pole1, pole2, pole4);
+                    child2.Method();
+                    Console.WriteLine(child2.Info());
+                }
+            }
+        }
+    }
+}
